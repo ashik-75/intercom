e21d21f1c11158d32de1f95f4c528955fac5a55e
Ashik <ashik@pop-os.localdomain>
2023-03-02 20:13:30 +0600

    ssds

--- app/blog/getBlog.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export async function getBlog() {
-  const blog = await axios.get("http://localhost:3000/api/blog");
-  return blog.data;
-}
--- app/blog/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getBlog } from "./getBlog";
-
-async function BlogPage() {
-  const blog = await getBlog();
-
-  return <div>BlogPage - {blog} </div>;
-}
-
-export default BlogPage;
